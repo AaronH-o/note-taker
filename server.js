c0269a84a28261dc625928ef66a1b3256c2909a2
Aaron Ho <bicecreamaar@gmail.com>
2024-05-02 17:38:47 -0700

    added post route for notes

--- server.js
+++ server.js
@@ -19,9 +19,31 @@ app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
+app.get('/api/notes', (req, res) => 
+  res.status(200).json(notes)
+);
 
+app.post('/api/notes', (req, res) => {
+    console.info(`${req.method} request received to add a note`);
+  const { title, text} = req.body;
 
+  if(title && text) {
+    const newNote = {
+      title,
+      text,
+    };
 
+    const response = {
+      status: 'success',
+      body: newNote,
+    };
+
+    console.log(response);
+    res.status(201).json(response)
+  } else {
+    res.status(500).json('Error in adding the note');
+  }
+  });
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
