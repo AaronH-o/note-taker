c7cd0ab1693523633afaa0188c83fe6b4ebed5f4
Aaron Ho <bicecreamaar@gmail.com>
2024-05-09 17:33:24 -0700

    post route now writes to the db.json

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
+const uuid = require('./helpers/uuid');
 const notes = require('./db/db');
 
 const PORT = 3001;
@@ -24,26 +26,45 @@ app.get('/api/notes', (req, res) =>
 );
 
 app.post('/api/notes', (req, res) => {
-    console.info(`${req.method} request received to add a note`);
+  console.info(`${req.method} request received to add a note`);
   const { title, text} = req.body;
 
   if(title && text) {
     const newNote = {
       title,
       text,
+      id: uuid(),
     };
+
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+      if(err) {
+        console.error(err);
+      } else {
+        const parsedNotes = JSON.parse(data);
+
+        parsedNotes.push(newNote);
+
+        fs.writeFile('./db/db.json',
+          JSON.stringify(parsedNotes, null, 4),
+          (writeErr) => 
+            writeErr
+              ? console.error(writeErr)
+              : console.info('Successfully updated reviews')
+        )
+      }
+    })
 
     const response = {
       status: 'success',
       body: newNote,
     };
 
-    console.log(response);
+    console.log("wow", response);
     res.status(201).json(response)
   } else {
     res.status(500).json('Error in adding the note');
   }
-  });
+});
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
