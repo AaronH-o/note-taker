8753f4d5a78821c3cf80cda9b869f2354ad0a33b
Aaron Ho <bicecreamaar@gmail.com>
2024-06-04 15:31:44 -0700

    added middleware

--- server.js
+++ server.js
@@ -2,11 +2,14 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const uuid = require('./helpers/uuid');
+const { clog } = require('./middleware/clog');
 const notes = require('./db/db');
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+
+app.use(clog);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
